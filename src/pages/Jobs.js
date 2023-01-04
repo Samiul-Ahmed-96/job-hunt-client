f74182a95d9dce4c8cc9dd346ffeef6ceaf9ccc3
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-01-04 13:03:06 +0600

    Updated Ui added job card

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,47 +1,25 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import JobCard from "../components/reusable/JobCard";
 import { useGetJobQuery } from "../features/job/jobApi";
 
 const Jobs = () => {
   const navigate = useNavigate();
   const { data, isLoading, isError } = useGetJobQuery();
 
-  return (
-    <div className="pt-14">
-      <div>
-        {data?.data?.map((singleJob) => (
-          <div
-            className=" bg-primary/10 inline-block rounded p-10 mr-2 "
-            key={singleJob._id}
-          >
-            <h1>Postion : {singleJob.position}</h1>
-            <p>Company Name :{singleJob.companyName}</p>
-            <h4>skills : </h4>
-            {singleJob?.skills.map((skill) => (
-              <p className="inline-block bg-white rounded mr-2 p-2 my-2" key={skill}>
-                {skill}
-              </p>
-            ))}
-            <h4>Requirements : </h4>
-            {singleJob?.requirements.map((requirement) => (
-              <p
-                className="inline-block bg-white rounded mr-2 p-2 my-2"
-                key={requirement}
-              >
-                {requirement}
-              </p>
-            ))} 
 
-            <button
-              className="border-primary border-2 border-solid p-2 mt-4 rounded hover:bg-white block"
-              onClick={() => navigate(`/job-details/${singleJob?._id}`)}
-            >
-              Details
-            </button>
-          </div>
-        ))}
-      </div>
+  return (
+    <div className='pt-14'>
+    <div className='bg-primary/10 p-5 rounded-2xl'>
+      <h1 className='font-semibold text-xl'>Find Jobs</h1>
     </div>
+    <div className='grid grid-cols-2 gap-5 mt-5'>
+      {
+        data?.data.map(singleJob => <JobCard jobData={singleJob} />)
+      }
+    </div>
+  </div>
+   
   );
 };
 
