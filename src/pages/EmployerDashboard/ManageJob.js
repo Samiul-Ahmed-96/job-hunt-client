42d15cb0a0fa8924697c1d28d25023ea7161986e
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-01-09 16:02:54 +0600

    filter jobs data based on email

--- src/pages/EmployerDashboard/ManageJob.js
+++ src/pages/EmployerDashboard/ManageJob.js
@@ -1,6 +1,7 @@
 import React from "react";
+import { FiTrash } from "react-icons/fi";
 import { useSelector } from "react-redux";
-import JobCard from "../../components/reusable/JobCard";
+import Loading from "../../components/reusable/Loading";
 import { useGetJobQuery } from "../../features/job/jobApi";
 
 const ManageJob = () => {
@@ -12,15 +13,65 @@ const ManageJob = () => {
   const filterEmployerJob = jobs.filter((job) => job?.email === user.email);
   console.log(filterEmployerJob);
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <div className="pt-14">
       <div className="bg-primary/10 p-5 rounded-2xl">
         <h1 className="font-semibold text-xl">Manage Job</h1>
       </div>
-      <div className="grid grid-cols-2 gap-5 mt-5">
-        {filterEmployerJob.map((job) => (
-          <JobCard jobData={job}></JobCard>
-        ))}
+      <div className="mt-10">
+        <div class="relative overflow-x-auto shadow-md sm:rounded-lg w-full">
+          <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead class="text-xs text-white uppercase bg-primary">
+              <tr>
+                <th scope="col" class="px-6 py-3">
+                  Position
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Company
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Experience
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Salary
+                </th>
+                <th scope="col" class="px-6 py-3">
+                  Action
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {filterEmployerJob.map((job) => (
+                <tr class="bg-white border-b  hover:bg-gray-50">
+                  <th
+                    scope="row"
+                    class="px-6 py-4 font-medium text-primary whitespace-nowrap"
+                  >
+                    {job?.position}
+                  </th>
+                  <td class="px-6 py-4">{job?.companyName}</td>
+                  <td class="px-6 py-4">{job?.experience}</td>
+                  <td class="px-6 py-4">{job?.salaryRange}</td>
+                  <td class="px-6 py-4 text-right">
+                    <button
+                      type="button"
+                      className="grid place-items-center rounded-full flex-shrink-0 bg-red-500/20 border border-red-500 h-11 w-11 group transition-all hover:bg-red-500"
+                    >
+                      <FiTrash
+                        className="text-red-500 group-hover:text-white transition-all"
+                        size="20"
+                      />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </div>
     </div>
   );
--- src/pages/candidates/Candidates.js
+++ src/pages/candidates/Candidates.js
@@ -6,23 +6,22 @@ import { useGetUserQuery } from "../../features/user/userApi";
 const Candidates = () => {
   const { data, isLoading, isFetching } = useGetUserQuery();
 
-
   const users = data?.data || [];
   const filterCandidates = users.filter((user) => user.role === "candidate");
 
-if(isLoading){
-    return <Loading/>
-}
+  if (isLoading) {
+    return <Loading />;
+  }
   return (
     <div className="pt-14">
       <div className="bg-primary/10 p-5 rounded-2xl">
         <h1 className="font-semibold text-xl">Candidates</h1>
       </div>
       <div className="grid grid-cols-2 gap-5 mt-5">
-      {
-        filterCandidates.map(candidate => <UserCard userData={candidate}></UserCard> )
-    }
-        </div>
+        {filterCandidates.map((candidate) => (
+          <UserCard userData={candidate}></UserCard>
+        ))}
+      </div>
     </div>
   );
 };
