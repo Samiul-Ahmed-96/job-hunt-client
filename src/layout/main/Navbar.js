17776adaed2f706967b0da15ec665179c855e4ba
Samiulahmedanik <84468942+SAANIK45@users.noreply.github.com>
2023-01-13 16:36:09 +0600

    fixed nav

--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -171,51 +171,50 @@ const Navbar = () => {
                 </ul>
               </div>
               <div className="-mr-2 flex md:hidden">
-              <button
-                onClick={() => setIsOpen(!isOpen)}
-                type="button"
-                className="bg-gray-900 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                <span className="sr-only">Open main menu</span>
-                {!isOpen ? (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M4 6h16M4 12h16M4 18h16"
-                    />
-                  </svg>
-                ) : (
-                  <svg
-                    className="block h-6 w-6"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M6 18L18 6M6 6l12 12"
-                    />
-                  </svg>
-                )}
-              </button>
+                <button
+                  onClick={() => setIsOpen(!isOpen)}
+                  type="button"
+                  className="bg-primary inline-flex items-center justify-center p-2 rounded-md text-white"
+                  aria-controls="mobile-menu"
+                  aria-expanded="false"
+                >
+                  <span className="sr-only">Open main menu</span>
+                  {!isOpen ? (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M4 6h16M4 12h16M4 18h16"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      className="block h-6 w-6"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M6 18L18 6M6 6l12 12"
+                      />
+                    </svg>
+                  )}
+                </button>
+              </div>
             </div>
-            </div>
-            
           </div>
         </div>
 
@@ -228,73 +227,67 @@ const Navbar = () => {
           leaveFrom="opacity-100 scale-100"
           leaveTo="opacity-0 scale-95"
         >
-          {(ref) => (
-            <div className="md:hidden" id="mobile-menu">
-              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                <li>
-                  <Link className="hover:text-primary" to="/employers">
-                    Employers
-                  </Link>
-                </li>
-                <li>
-                  <Link className="hover:text-primary" to="/candidates">
-                    Candidates
-                  </Link>
-                </li>
-                <li>
-                  <Link className="hover:text-primary" to="/jobs">
-                    Jobs
-                  </Link>
-                </li>
-                {email && role && (
-                  <li>
-                    <Link
-                      className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
-                      to="/dashboard"
-                    >
-                      Dashboard
+          {(ref) => ( 
+            <div className="md:hidden container ml-16" id="mobile-menu">
+              <div ref={ref} className="pt-2 pb-3">
+                <ul>
+                  <li className="mt-1">
+                    <Link className="hover:text-primary" to="/employers">
+                      Employers
                     </Link>
                   </li>
-                )}
+                  <li className="mt-1">
+                    <Link className="hover:text-primary" to="/candidates">
+                      Candidates
+                    </Link>
+                  </li>
+                  <li className="mt-1">
+                    <Link className="hover:text-primary" to="/jobs">
+                      Jobs
+                    </Link>
+                  </li>
+                  {email && role && (
+                    <li className="mt-1">
+                      <Link className="hover:text-primary" to="/dashboard">
+                        Dashboard
+                      </Link>
+                    </li>
+                  )}
 
-                {email && (
-                  <Link
-                    title="Go To Chat"
-                    className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all"
-                    to={"/chat"}
-                  >
-                    <BsChatDots />
-                  </Link>
-                )}
+                  {email && (
+                    <li className="mt-1">
+                      <Link className="hover:text-primary" to="/chat">
+                        Chat
+                      </Link>
+                    </li>
+                  )}
 
-                {email && !role && (
-                  <li>
-                    <Link
-                      className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
-                      to="/register"
+                  {email && !role && (
+                    <li className="mt-1">
+                      <Link className="hover:text-primary" to="/register">
+                        Get Started
+                      </Link>
+                    </li>
+                  )}
+                  {email ? (
+                    <button
+                      title="Logout"
+                      onClick={handleLogout}
+                      className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all"
                     >
-                      Get Started
-                    </Link>
-                  </li>
-                )}
-                {email ? (
-                  <button
-                    title="Logout"
-                    onClick={handleLogout}
-                    className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all"
-                  >
-                    <FiLogOut />
-                  </button>
-                ) : (
-                  <li>
-                    <Link
-                      className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
-                      to="/login"
-                    >
-                      Login
-                    </Link>
-                  </li>
-                )}
+                      <FiLogOut />
+                    </button>
+                  ) : (
+                    <li className="mt-1">
+                      <Link
+                        className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+                        to="/login"
+                      >
+                        Login
+                      </Link>
+                    </li>
+                  )}
+                </ul>
               </div>
             </div>
           )}
